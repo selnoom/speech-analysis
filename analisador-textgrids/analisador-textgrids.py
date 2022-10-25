436ab81eb5968c798a1ba369948586c872888f67
nicholas P <nicholas@usp.br>
2022-10-24 22:15:49 -0300

    Introduz media de palavras por segmento

--- analisador-textgrids/analisador-textgrids.py
+++ analisador-textgrids/analisador-textgrids.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import re 
 import sys
 from cProfile import label
 from os.path import join
@@ -6,8 +7,8 @@ from praatio import textgrid
 
 # python myscript.py > myoutput.txt
 # Pasta com TextGrids
-# dir_path = r'arquivos/NTB'
-dir_path = sys.argv[1]
+dir_path = r'/home/nicholasp/Documentos/tcc/git/speech-analysis/analisador-textgrids/arquivos/TB'
+# dir_path = sys.argv[1]
 
 arquivosLista = []
 
@@ -29,26 +30,29 @@ for arquivo in arquivosLista:
 
     tierList = []
     for tier in tg.tierNameList:
-        # Remover comentários dos textgrids
+        # Remove layer de comentários
         if tier != 'com':
             tierList.append(tg.tierDict[tier])
 
     durationList = []
     pausaList = []
+    palavrasList = []
 
     for tierDict in tierList:
         for start, stop, label in tierDict.entryList:
             if '...' not in label:
                 durationList.append(stop - start)
+                if (re.search('[a-zA-Z]', label)):
+                    palavrasList.append(len(label.split()))
             elif label == '...':
                 pausaList.append(stop - start)
 
-    segmentoMaximo = max(durationList)
-    segmentoMinimo = min(durationList)
+    segmentoMax = max(durationList)
+    segmentoMin = min(durationList)
     segmentoMedia = Average(durationList)
     print(f'Tempo médio dos segmentos: {segmentoMedia}')
-    print(f'Tempo máximo dos segmentos: {segmentoMaximo}')
-    print(f'Tempo mínimo dos segmentos: {segmentoMinimo}\n')
+    print(f'Tempo máximo dos segmentos: {segmentoMax}')
+    print(f'Tempo mínimo dos segmentos: {segmentoMin}\n')
 
     pausaMax = max(pausaList)
     pausaMin = min(pausaList)
@@ -56,4 +60,12 @@ for arquivo in arquivosLista:
     print(f'Tempo médio das pausas: {pausaMedia}')
     print(f'Tempo máximo das pausas: {pausaMax}')
     print(f'Tempo mínimo das pausas: {pausaMin}\n')
+
+    palavrasMedia = round(Average(palavrasList))
+    palavrasMax= max(palavrasList)
+    palavrasMin = min(palavrasList)
+    print(f'Média das palavras: {palavrasMedia}')
+    print(f'Máximo das palavras: {palavrasMax}')
+    print(f'Mínimo das palavras: {palavrasMin}\n')
+
     print('------------------------------------------\n')
