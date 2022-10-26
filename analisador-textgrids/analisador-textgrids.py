ca0f01dc6f34b9232c0dc87130964f29a61b2989
nicholas P <nicholas@usp.br>
2022-10-25 21:11:48 -0300

    Adiciona analise da pasta como um todo e remove arquivos fora de lugar

--- analisador-textgrids/analisador-textgrids.py
+++ analisador-textgrids/analisador-textgrids.py
@@ -4,13 +4,19 @@ import sys
 from cProfile import label
 from os.path import join
 from praatio import textgrid
+from listas.pausasPreenchidas import PausasPreenchidas
 
 # python myscript.py > myoutput.txt
+
 # Pasta com TextGrids
 dir_path = r'/home/nicholasp/Documentos/tcc/git/speech-analysis/analisador-textgrids/arquivos/TB'
 # dir_path = sys.argv[1]
 
 arquivosLista = []
+pausasPreenchidas = [item.name for item in PausasPreenchidas]
+durationListTotal = []
+pausaListTotal = []
+palavrasListTotal= []
 
 for path in os.listdir(dir_path):
     if os.path.isfile(os.path.join(dir_path, path)):
@@ -25,14 +31,15 @@ for arquivo in arquivosLista:
     print(f'Arquivo: {inputFN}')
 
     tg = textgrid.openTextgrid(inputFN, includeEmptyIntervals=False) 
-
+    # Remove layer de comentários
+    if 'com' in tg.tierNameList:
+        tg.tierNameList.remove('com')
+    
     print(f'Tiers: {tg.tierNameList}')
 
     tierList = []
     for tier in tg.tierNameList:
-        # Remove layer de comentários
-        if tier != 'com':
-            tierList.append(tg.tierDict[tier])
+        tierList.append(tg.tierDict[tier])
 
     durationList = []
     pausaList = []
@@ -40,12 +47,15 @@ for arquivo in arquivosLista:
 
     for tierDict in tierList:
         for start, stop, label in tierDict.entryList:
-            if '...' not in label:
+            if '...' not in label and label not in pausasPreenchidas:
                 durationList.append(stop - start)
                 if (re.search('[a-zA-Z]', label)):
                     palavrasList.append(len(label.split()))
             elif label == '...':
                 pausaList.append(stop - start)
+    durationListTotal += durationList
+    pausaListTotal += pausaList
+    palavrasListTotal += palavrasList
 
     segmentoMax = max(durationList)
     segmentoMin = min(durationList)
@@ -66,6 +76,12 @@ for arquivo in arquivosLista:
     palavrasMin = min(palavrasList)
     print(f'Média das palavras: {palavrasMedia}')
     print(f'Máximo das palavras: {palavrasMax}')
-    print(f'Mínimo das palavras: {palavrasMin}\n')
+    print(f'Mínimo das palavras: {palavrasMin}')
+    print(f'{palavrasList}\n')
 
     print('------------------------------------------\n')
+
+print('Média de todos os arquvios:')
+print(f'Tempo médio de segmento: {Average(durationListTotal)}')
+print(f'Tempo médio de pausa: {Average(pausaListTotal)}')
+print(f'Média de palavras por segmento (arredondando): {round(Average(palavrasList))}')
---
+++ analisador-textgrids/listas/pausasPreenchidas.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class PausasPreenchidas(Enum):
+    ahn = 0,
+    ah = 1,
+    hun = 2,
+    oh = 3,
+    hunun = 4,
+    hum = 5,
+    hein = 6,
+    uhn = 7,
+    htum = 8,
+    hinhun = 9,
+    uhm = 10,
+    hanhan = 11,
+    han = 12,
+    ohr = 13,
+    ha = 14,
+    eh = 15,
+    uhum = 16,
+    ehn = 17,
+    éh = 18
